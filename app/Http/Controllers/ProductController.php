ac9937f9727af1d9b8df8bf4bec71be716516750
faqih123-art <faqih7571@gmail.com>
2025-05-12 23:47:10 +0700

    first commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends Controller
     }
 
     public function create()
-    {
+    { 
         $categories = Categories::all(); // Ambil semua kategori
         return view('dashboard.products.create', compact('categories'));
     }
